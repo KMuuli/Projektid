fc3c0394af874e18bcffeecea40ca83fe8ad9381
Kaspar Muuli <99071819+KMuuli@users.noreply.github.com>
2023-10-08 13:00:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Projektid
+{
+    class Projektid
+    {
+        static void Main(string[] args)
+        {
+            // GChange the appearance
+
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            // Get a conversation going
+
+            Console.WriteLine("Hello, whats your name?");
+
+            Console.ReadLine(); // This works too
+
+            Console.WriteLine("My name is RX-9000. \n Im an AI sent from the future to destroy mankind.");
+            Console.WriteLine("What is yout favorite color?");
+
+            Console.ReadLine();
+            Console.WriteLine("Cool! Mine is destruction.");
+
+            Console.ReadKey();
+        }
+
+    }
+}
